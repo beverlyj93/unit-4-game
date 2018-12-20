a4e3c8aea16698c824084b2a98f49b283075b507
Joshua Beverly <jbeverly@heliontechnologies.com>
2018-12-20 09:19:54 -0600

    Second commit.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,8 @@
 // Global Variables
-var randomNumber = 0;
+var randomScore = 0;
+var currentScore = 0;
+var wins = 0;
+var losses = 0;
 
 // On Page Load
 $(document).ready(function() {
@@ -7,13 +10,31 @@ $(document).ready(function() {
 })
 
 $('img').click(function() {
-    
+    currentScore += parseInt($(this).attr("value"));
+    $('#score').text(parseInt(currentScore));
+
+    if(currentScore == randomScore) {
+        wins++;
+        $('#win').text(wins);
+        onGameLoad();
+    }
+    else if(currentScore > randomScore) {
+        losses++;
+        $('#loss').text(losses);
+        onGameLoad();
+    }
 })
 
 // Functions
 
 function onGameLoad() {
+    currentScore = 0;
+    $('#score').text("0");
+
     for(var i = 1; i < 5; i++) {
         $('#crystal-' + i).attr("value", Math.floor(Math.random() * 10));
     }
+    randomScore = Math.floor(Math.random() * 99);
+
+    $('.random-number').text(randomScore);
 }
